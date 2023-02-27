f2d7f3a8286dc1a6fd238693bfb024d1c79167fd
peeptoad <112097932+peeptoad@users.noreply.github.com>
2023-02-27 12:27:43 -0600

    Create receipt.js

---
+++ receipt.js
@@ -0,0 +1,11 @@
+ 
+
+Function greet() {
+
+ if (hrs < 12)
+        alert("Good Morning");
+    else if (hrs >= 12 && hrs <= 17)
+        alert("Good Afternoon");
+    else if (hrs >= 17 && hrs <= 24)
+        alert("Good Evening");
+}
